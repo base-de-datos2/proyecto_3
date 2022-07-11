caf78945f6d085f07e37e5b110906f6be2257441
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-07-10 20:43:59 -0500

    updated frontend

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from venv import create
 from flask import Flask, render_template, jsonify,request,redirect, send_from_directory,send_file
 import psycopg2
-from PIL import Image
 import time
 import face_recognition
 from utility_functions import ImgObject
@@ -15,12 +14,7 @@ app = Flask(__name__)
 with open('vector_imgs.npy',"rb") as file:
         imgs_encodings = np.load(file,allow_pickle=True)
 
-faiss_index= faiss.IndexHNSWFlat(128,32)
-
-for i in range(len(imgs_encodings)):
-    local_img = imgs_encodings[i]
-    faiss_index.add(np.array([local_img.file_img_encoding],np.float32))
-
+faiss_index= faiss.read_index("faiss_index.dat")
 
 
 @app.route('/')
@@ -46,35 +40,25 @@ def knn_query():
 @app.route('/get-img/<filename>')
 def send_img(filename):
     filename = filename.replace('-','/')
-    print(filename)
+    # print(filename)
     return send_file("./" + filename)
 
 @app.route('/make-knn-query',methods=['POST'])
 def query():
     print(request)
-    print(request.files)
     file = request.files['file']
-    print(request.get_data())
     img = face_recognition.load_image_file(file)
     img_encoding = face_recognition.face_encodings(img)
-    _,I = faiss_index.search(np.array([img_encoding[0]],np.float32),10)
+    _,I = faiss_index.search(np.array([img_encoding[0]],np.float32),int(request.form['k']))
     faiss_results = []
 
     for result in I[0]:
         path = imgs_encodings[result].path[2:].replace('/','-')
-        # print(path)
-        faiss_results.append(path)
-    # text = request.args.get('query')
-    # k = request.args.get('k')
-    # start = time.time()
-    # keys, dict = create_tf_query(procesamiento(text))
-    # create_unit_vector_query('sorted_tokens.txt', 2507 -1 , keys, dict)
-    # tweets = topk(2507 -1, int(k))
-
-    # tiempo_spimi = time.time() - start
+        name = imgs_encodings[result].dir.replace('_',' ')
+        faiss_results.append([path,name])
 
 
-    # return render_template('topk.html', tweets = tweets, tweets_postgres = query_postgres_result, tiempo_postgres = tiempo_postgres, tiempo_spimi = tiempo_spimi)
+
     return render_template('display_knn_query.html',faiss_imgs_paths = faiss_results) 
 
 
