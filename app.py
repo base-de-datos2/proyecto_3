5c39ea5a82818b7f1457ed49eaf75de42252fbac
panchomb <francisco.magot@utec.edu.pe>
2022-07-10 21:25:56 -0500

    new searches

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from venv import create
 from flask import Flask, render_template, jsonify,request,redirect, send_from_directory,send_file
-import psycopg2
-from PIL import Image
 import time
 import face_recognition
 from utility_functions import ImgObject
 import numpy as np
+from queue import PriorityQueue
+from rtree import index as rtindex
 import faiss
 
 
@@ -45,9 +45,8 @@ def knn_query():
 
 @app.route('/get-img/<filename>')
 def send_img(filename):
-    filename = filename.replace('-','/')
-    print(filename)
-    return send_file("./" + filename)
+    filename = filename.replace('}','/')
+    return send_file(filename)
 
 @app.route('/make-knn-query',methods=['POST'])
 def query():
@@ -57,25 +56,57 @@ def query():
     print(request.get_data())
     img = face_recognition.load_image_file(file)
     img_encoding = face_recognition.face_encodings(img)
-    _,I = faiss_index.search(np.array([img_encoding[0]],np.float32),10)
+    _,I = faiss_index.search(np.array([img_encoding[0]],np.float32),int(request.form['k']))
     faiss_results = []
 
     for result in I[0]:
-        path = imgs_encodings[result].path[2:].replace('/','-')
+        path = imgs_encodings[result].path[2:].replace('/','}')
         # print(path)
         faiss_results.append(path)
-    # text = request.args.get('query')
-    # k = request.args.get('k')
-    # start = time.time()
-    # keys, dict = create_tf_query(procesamiento(text))
-    # create_unit_vector_query('sorted_tokens.txt', 2507 -1 , keys, dict)
-    # tweets = topk(2507 -1, int(k))
-
-    # tiempo_spimi = time.time() - start
 
 
-    # return render_template('topk.html', tweets = tweets, tweets_postgres = query_postgres_result, tiempo_postgres = tiempo_postgres, tiempo_spimi = tiempo_spimi)
-    return render_template('display_knn_query.html',faiss_imgs_paths = faiss_results) 
+    # Rtree
+    p = rtindex.Property()
+    p.dimension = 128
+    p.dat_extension = "data"
+    p.idx_extension = "index"
+
+    idx = rtindex.Index("puntos", properties = p)
+
+    rtree_knn = idx.nearest(img_encoding[0], num_results = int(request.form['k']))
+    rtree_results = []
+
+    for result in rtree_knn:
+        path = imgs_encodings[result].path[2:].replace('/', '}')
+        rtree_results.append(path)
+
+
+    # Sequential
+    top_k = PriorityQueue()
+
+    for i in range(len(imgs_encodings) - 1):
+        curr_encoding = imgs_encodings[i].file_img_encoding
+
+        dist = face_recognition.face_distance([curr_encoding], img_encoding[0])
+
+        if len(top_k.queue) < int(request.form['k']):
+            top_k.put((-1 * dist, i))
+        else:
+            top = top_k.queue[0]
+            if -1 * dist > top[0]:
+                top_k.get()
+                top_k.put((-1 * dist, i))
+
+    sequential_results = []
+    for i in range(len(top_k.queue)):
+        top = top_k.queue[0]
+        print(top)
+        sequential_index = top[1]
+        path = imgs_encodings[sequential_index].path[2:].replace('/', '}')
+        sequential_results = [path] + sequential_results
+        top_k.get()
+
+    return render_template('display_knn_query.html',faiss_imgs_paths = faiss_results, sequential_imgs_paths = sequential_results, rtree_imgs_paths = rtree_results) 
 
 
 if __name__ == '__main__':
